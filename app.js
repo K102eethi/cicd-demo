b10589e1e1c4f1a38c5e37e323ebb8c3486cd2c9
K102eethi <pallerlakeerthireddy222@gmail.com>
2025-08-08 11:00:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
  
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline,Completed!');
+  res.send('Hello from CI/CD Pipeline,Completed,Thanks!');
 });
  
 app.listen(port, () => {
